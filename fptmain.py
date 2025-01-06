ddf4956342e3b1dfd998b5bc8df6f54b374cd107
pejalgit71 <160094060+pejalgit71@users.noreply.github.com>
2025-01-06 17:06:36 +0800

    Update fptmain.py

--- fptmain.py
+++ fptmain.py
@@ -25,31 +25,7 @@ menu = st.sidebar.radio("Select a Function", [
 # Main Content
 if menu == "Introduction":
     st.title("IoT and EoT Security Framework Simulation")
-#     st.write("""
-#     <!DOCTYPE html>
-#     <html>
-#     <title>W3.CSS</title>
-#     <meta name="viewport" content="width=device-width, initial-scale=1">
-#     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-#     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
-#     <style>
-#     .w3-tangerine {
-#       font-family: "Tangerine", serif;
-#     }
-#     </style>
-#     <body>
-    
-#     <div class="w3-container w3-tangerine">
-#       <p class="w3-xxlarge">Making the Web Beautiful!</p>
-#       <p class="w3-xxxlarge">Making the Web Beautiful!</p>
-#       <p class="w3-jumbo">Making the Web Beautiful!</p>
-#     </div>
 
-#     </body>
-# </html> 
-#     Welcome to the IoT and EoT Security Framework Simulation.  
-#     Use the navigation on the left to explore different functions.
-#     """)
     html_content = """
     <!DOCTYPE html>
     <html>
@@ -65,9 +41,9 @@ if menu == "Introduction":
     <body>
     
     <div class="w3-container w3-tangerine">
-      <p class="w3-xxlarge">Making the Web Beautiful!</p>
+      // <p class="w3-xxlarge">Making the Web Beautiful!</p>
       <p class="w3-xxxlarge">Making the Web Beautiful!</p>
-      <p class="w3-jumbo">Making the Web Beautiful!</p>
+      // <p class="w3-jumbo">Making the Web Beautiful!</p>
     </div>
     
     </body>
